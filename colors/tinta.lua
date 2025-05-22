27420a285c22a68ca6c4415b8d13093c98122d1d
Ardi Nugraha <ardi.nugraha@bvarta.com>
2025-05-22 08:58:37 +0700

    add debugger message for entrypoint

--- colors/tinta.lua
+++ colors/tinta.lua
@@ -1,2 +1,6 @@
-local tinta = require("tinta.init")
-tinta.load()
+local ok, tinta = pcall(require, "tinta")
+if not ok then
+	vim.notify("Failed to load tinta module: " .. tostring(tinta), vim.log.levels.ERROR)
+else
+	tinta.load()
+end
