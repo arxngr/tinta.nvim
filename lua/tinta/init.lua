c46a1e0ca18ddaa809b5c2fe4146d92558275f65
ardinugraha <ardi.nugraha@bvarta.com>
2025-08-28 14:25:46 +0700

    fix: override the pallete rather initiate new color if using bg_color

--- lua/tinta/init.lua
+++ lua/tinta/init.lua
@@ -16,10 +16,16 @@ M.config = defaultConfig
 function M.setup(options)
 	M.config = vim.tbl_deep_extend("force", {}, defaultConfig, options or {})
 
-	-- Set the Cursor highlight group: blackish text, blue background
+	-- Override pallete before theme loads
+	local pallete = require("highlights.tinta")
+	if M.config.bg_color and M.config.bg_color ~= "" then
+		pallete.bg = M.config.bg_color
+	end
+
+	-- Set Cursor highlight
 	vim.cmd("highlight Cursor guifg=#0f1013 guibg=#10B1FE")
 
-	-- Set the guicursor option with the right modes and Cursor highlight
+	-- Set guicursor option
 	if M.config.guicursor then
 		vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
 	end
@@ -42,22 +48,6 @@ function M.load()
 				NvimTreeNormalNC({ bg = "NONE" }),
 				BufferInactive({ theme.BufferInactive, bg = "NONE" }),
 				BufferVisible({ theme.BufferCurrent }),
-			}
-		end)
-	end
-
-	-- Apply user-defined background only if bg_color is not empty
-	if M.config.bg_color ~= "" then
-		theme = lush.extends({ theme }).with(function()
-			return {
-				Normal({ bg = M.config.bg_color }),
-				NormalSB({ bg = M.config.bg_color }),
-				BufferlineFill({ bg = M.config.bg_color }),
-				TroubleNormal({ bg = M.config.bg_color }),
-				NvimTreeNormal({ bg = M.config.bg_color }),
-				NvimTreeNormalNC({ bg = M.config.bg_color }),
-				BufferInactive({ bg = M.config.bg_color }),
-				BufferVisible({ bg = M.config.bg_color }),
 			}
 		end)
 	end
